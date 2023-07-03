c57ea025aff1b5664d04387581ac90fb92847ef6
vbnm95 <vbnm95@naver.com>
2023-07-03 12:53:39 +0900

    #을 @로 변경

--- Triangle.java
+++ Triangle.java
@@ -2,7 +2,7 @@ class Triangle {
 	
 	public static void main(String[] args) {
 		for(int i=1; i<11; i++)
-			System.out.println("#".repeat(i));	
+			System.out.println("@".repeat(i));	
 		
 		System.out.println("Triangle");	
 	}
