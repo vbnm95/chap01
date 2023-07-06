930cd3e9d5c66490d6d9b47ba97c27204535e08c
vbnm95 <vbnm95@naver.com>
2023-07-06 12:01:11 +0900

    vt100 clear

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
-class Hello {
+public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
+		System.out.println("\033[2J");
+		System.out.println("Hello, Java");
 	}
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,6 +1,7 @@
 class Triangle {
 	
 	public static void main(String[] args) {
+		//삼각형 출력
 		for(int i=1; i<11; i++)
 			System.out.println("@".repeat(10-i));	
 		
