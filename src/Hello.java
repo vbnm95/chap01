5aefb4d1e8e8c9d0bea7e07a16f69ab3ad7cdb7f
vbnm95 <vbnm95@naver.com>
2023-07-06 13:02:05 +0900

    color fg bg 추가 변경

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,27 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("\033[2J");
+		
 		System.out.println("\033[31m");
 		System.out.println("\033[44m");
-		System.out.println("Hello, Java");
+		System.out.print("\033[10;20H");
+		System.out.println("Hello, Java!");
+		
+		System.out.println("\033[34m");
+		System.out.println("\033[41m");
+		System.out.print("\033[5;10H");
+		System.out.println("Hello, JavaScript!");
+
+		System.out.println("\033[35m");
+		System.out.println("\033[42m");
+		System.out.print("\033[15;5H");
+		System.out.println("Hello, HTML!");
+
+		System.out.println("\033[35m");
+		System.out.println("\033[31m");
+		System.out.print("\033[13;35H");
+		System.out.println("Hello, SQL!");
+
 		System.out.println("\033[0m");
 	}
 }
