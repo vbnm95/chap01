d98621c40495689dcbf2e309191f1332c4369b87
vbnm95 <vbnm95@naver.com>
2023-07-12 09:10:12 +0900

    주석 관련 내용 변경

---
+++ src/Comment.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * 주석처리 방법 연구 <br>
+ * 1. 한줄 주석 기호 // <br>
+ * 2. 여러줄 주석 기호 <br>
+ * 3. 자바덕 주석 기호 <br>
+ * 
+ */
+public class Comment {
+	/*
+	 * main 메소드는 entry point라고 하며
+	 * 실행 클래스에는 반드시 있어야 한다.
+	 * 
+	 * 한줄 지우기: ctrl + D
+	 * 한줄 주석 단축키 : ctrl + /
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.print("Hi");	// print는 new line을 삽입하지 않는다.
+		//System.out.println("Hello Java");
+		System.out.print("Hi");
+	}
+
+}
